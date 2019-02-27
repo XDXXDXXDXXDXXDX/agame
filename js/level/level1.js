f027e4349584fb326b4fa547fd2942bf47a033e3
XDXXDXXDXXDXXDX <40033292+XDXXDXXDXXDXXDX@users.noreply.github.com>
2019-02-27 22:45:43 +0800

    Add files via upload

--- js/level/level1.js
+++ js/level/level1.js
@@ -2,6 +2,7 @@
  * 反射物体需要按照可能性存入数组中
  */
 let Level1 = {
+    bgMusic: 'bgMusic1',
     laserTransmitter: [
         {
             name: 'laserA',
@@ -14,8 +15,8 @@ let Level1 = {
             height: Config.laserTransmitterSize.height // 发射器高度
         }
     ],
-    lightHome: [
-        {
+    // 终点限制一个
+    lightHome: {
             name: 'lightHomeA',
             x: 150, // 发射器x坐标，激光开始的x坐标
             y: 400, // 发射器y坐标，激光结束的y坐标
@@ -23,27 +24,8 @@ let Level1 = {
             icon: imgBox['lightHome'], // 发射器图标
             width: Config.lightHomeSize.width, // 发射器宽度
             height: Config.lightHomeSize.height // 发射器高度
-        },
-        {
-            name: 'lightHomeB',
-            x: 250, // 发射器x坐标，激光开始的x坐标
-            y: 300, // 发射器y坐标，激光结束的y坐标
-            deg: 0,
-            icon: imgBox['lightHome'], // 发射器图标
-            width: Config.lightHomeSize.width, // 发射器宽度
-            height: Config.lightHomeSize.height // 发射器高度
-        }
-    ],
-    mirror: [
-        {
-            name: 'mirrorAA',
-            x: 320, // 发射器x坐标，激光开始的x坐标
-            y: 200, // 发射器y坐标，激光结束的y坐标
-            deg: 45,
-            icon: imgBox['mirror'], // 发射器图标
-            width: Config.mirrorSize.width, // 发射器宽度
-            height: Config.mirrorSize.height // 发射器高度
-        },
+    },
+    mirror: [   
         {
             name: 'mirrorA',
             x: 280, // 发射器x坐标，激光开始的x坐标
@@ -52,7 +34,16 @@ let Level1 = {
             icon: imgBox['mirror'], // 发射器图标
             width: Config.mirrorSize.width, // 发射器宽度
             height: Config.mirrorSize.height // 发射器高度
-        },       
+        },   
+        {
+            name: 'mirrorAA',
+            x: 320, // 发射器x坐标，激光开始的x坐标
+            y: 220, // 发射器y坐标，激光结束的y坐标
+            deg: 60,
+            icon: imgBox['mirror'], // 发射器图标
+            width: Config.mirrorSize.width, // 发射器宽度
+            height: Config.mirrorSize.height // 发射器高度
+        },    
         {
             name: 'mirrorB',
             x: 180, // 发射器x坐标，激光开始的x坐标
