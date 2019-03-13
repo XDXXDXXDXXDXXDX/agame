85ff5585c7dd4609e43777486d9a3e6b96140801
xdxxdxxdxxdxxdx <xdx1770796956@gmail.com>
2019-03-13 23:02:44 +0800

    1、删除就方法

--- js/element/wall.js
+++ js/element/wall.js
@@ -19,41 +19,6 @@ class Wall {
             for(let [i, path] of line.entries()) {
                 let i2 = i + 1 == line.length ? 0 : i + 1;
                 bricks = bricks.concat(bricksFactory.call(this, path, line[i2], true));
-                // bricks.concat([3,4,5])
-                // console.log(bricksFactory.call(this, path, line[i2], true))
-                // console.log(bricks)
-                // let dx = (line[i2].x - path.x) * Config.window.scale;
-                // let dy = (line[i2].y - path.y) * Config.window.scale;
-                // let adx = Math.abs(dx);
-                // let ady = Math.abs(dy);
-                // let _x = dx == 0 ? 1 : dx / adx; // x轴是否递增
-                // let _y = dy == 0 ? 1 : dy / ady; // y轴是否递增
-                // let xOy = adx > ady ? true : false; // 以x或y为尺度（选为尺度的将以1分割，另外一个以尺度分割的个数来确定分割的单位，true为x,false为y）
-                // let d = xOy ? ady / adx : adx / ady;
-    
-                // if(xOy) {
-                //     for(let i = 0; i < adx; i++) {
-                //         bricks.push(new Brick({
-                //             name: wall.name + i,
-                //             x: path.x * Config.window.scale + i * _x,
-                //             y: path.y * Config.window.scale + d * i * _y,
-                //             endX: path.x * Config.window.scale + (i + 1) * _x,
-                //             endY: path.y * Config.window.scale + d * (i + 1) * _y,
-                //             ori: this
-                //         }));
-                //     }
-                // }else{
-                //     for(let i = 0; i < ady; i++) {
-                //         bricks.push(new Brick({
-                //             name: wall.name + i,
-                //             x: path.x * Config.window.scale + d * i * _x,
-                //             y: path.y * Config.window.scale + i * _y,
-                //             endX: path.x * Config.window.scale + d * (i + 1) * _x,
-                //             endY: path.y * Config.window.scale + (i + 1) * _y,
-                //             ori: this
-                //         }));
-                //     }
-                // }
             }
         }
         return bricks;
